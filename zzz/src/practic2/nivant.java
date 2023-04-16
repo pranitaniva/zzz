8f560ac61b85f66f86c1fe32cf06312737ca5a09
pranitaniva <130834667+pranitaniva@users.noreply.github.com>
2023-04-16 15:32:31 +0530

    Update nivant.java

--- zzz/src/practic2/nivant.java
+++ zzz/src/practic2/nivant.java
@@ -8,5 +8,6 @@ public static void main(String[] args)
 	System.out.println("hi to all");
 	System.out.println("print all odd no");
 	System.out.println("hi to all");
+	System.out.println("hi to all");
 }
 }
